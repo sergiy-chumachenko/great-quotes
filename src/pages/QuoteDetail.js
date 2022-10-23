1393efae53c941f0d364f9a99077338c05686f82
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-10-23 16:48:56 +0300

    writing more flexible routing code

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,4 @@
-import {Link, Route} from "react-router-dom";
-import {useParams} from "react-router-dom/cjs/react-router-dom";
+import {Link, Route, useParams, useRouteMatch} from "react-router-dom";
 import {Fragment} from "react";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -11,6 +10,8 @@ const DUMMY_QUOTES = [
 
 
 const QuoteDetail = () => {
+    const match = useRouteMatch();
+    console.log(match);
     const params = useParams();
     const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
     if (!quote) {
@@ -19,12 +20,12 @@ const QuoteDetail = () => {
     return (
         <Fragment>
             <HighlightedQuote text={quote.text} author={quote.author}/>
-            <Route path={`/quotes/${params.quoteId}`} exact>
+            <Route path={match.path} exact>
                 <div className="centered">
-                    <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+                    <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
                 </div>
             </Route>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
+            <Route path={`${match.path}/comments`}>
                 <Comments/>
             </Route>
         </Fragment>
